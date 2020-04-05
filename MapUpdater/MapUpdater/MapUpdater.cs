db08a9708cd49b6c0ed7cffaaeb755c13f26c878
FrostBird347 <39435218+FrostBird347@users.noreply.github.com>
2020-04-05 20:45:01 +0800

    Updated DMP plugin JSON format sent and make it also send the vessel ID
    I am going to try to use the vessel ID to update the markers, instead of removing and replacing them, resulting in the markers staying selected when an update occurs,

--- MapUpdater/MapUpdater/MapUpdater.cs
+++ MapUpdater/MapUpdater/MapUpdater.cs
@@ -1,4 +1,4 @@
-//Originally, this plugin was just going to be used for a private server, because of that, a lot of older code has/is being changed to be usable for other people.
+//Originally, this plugin was just going to be used for a private server, because of that, a lot of older code has been changed to be usable for other people.
 using System;
 using System.Collections.Specialized;
 using System.Net;
@@ -24,8 +24,7 @@ namespace MapJSONUpdate
     {
 
         private int updateCallCount = 0;
-        public String NormalVesselsList;
-        public String DebrisVesselsList;
+        public String VesselsList;
         public String FinalSentVesselsList;
         private int ServerClockLast = 0;
 
@@ -46,45 +45,24 @@ namespace MapJSONUpdate
             {
                 updateCallCount = 0;
                 //TODO: use actual JSON, instead of creating and sending a string.
-                NormalVesselsList = ",\"Vessels\":{\"ID\":[";
-                DebrisVesselsList = "\"Debris\":{ \"ID\":[";
+                VesselsList = "{\"Main\":{\"ID\":[";
                 //Modified version of DarkMultiPlayerServer.Dekessler().
                 //The private server had a "public frequency", which was meant to be the only visible thing on the map, it has since been removed, but I likely will end up adding it back in a separate version for my server, so I have kept the code here: c3RyaW5nIGN1cnJlbnRMaW5lID0gc3IuUmVhZExpbmUoKTsKICAgICAgICAgICAgICAgICAgICAgICAgd2hpbGUgKGN1cnJlbnRMaW5lICE9IG51bGwgJiYgIXZlc3NlbElzUHVibGljRnJlcSkKICAgICAgICAgICAgICAgICAgICAgICAgewogICAgICAgICAgICAgICAgICAgICAgICAgICAgc3RyaW5nIHRyaW1tZWRMaW5lID0gY3VycmVudExpbmUuVHJpbSgpOwogICAgICAgICAgICAgICAgICAgICAgICAgICAgaWYgKHRyaW1tZWRMaW5lLlRyaW0oKS5TdGFydHNXaXRoKCJGcmVxdWVuY3kgPSIsIFN0cmluZ0NvbXBhcmlzb24uT3JkaW5hbCkpCiAgICAgICAgICAgICAgICAgICAgICAgICAgICB7CiAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgc3RyaW5nIFZlc3NlbEZyZXF1ZW5jeSA9IHRyaW1tZWRMaW5lLlN1YnN0cmluZyh0cmltbWVkTGluZS5JbmRleE9mKCI9IiwgU3RyaW5nQ29tcGFyaXNvbi5PcmRpbmFsKSArIDIpOwogICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgIGlmIChWZXNzZWxGcmVxdWVuY3kgPT0gIjIwIikKICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICB7CiAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgIHZlc3NlbElzUHVibGljRnJlcSA9IHRydWU7CiAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgfQogICAgICAgICAgICAgICAgICAgICAgICAgICAgfQogICAgICAgICAgICAgICAgICAgICAgICAgICAgY3VycmVudExpbmUgPSBzci5SZWFkTGluZSgpOwogICAgICAgICAgICAgICAgICAgICAgICB9CiAgICAgICAgICAgICAgICAgICAgfQ==
                 string[] FullvesselList = Directory.GetFiles(Path.Combine(Server.universeDirectory, "Vessels"));
                 foreach (string vesselFile in FullvesselList)
                 {
                     string vesselID = Path.GetFileNameWithoutExtension(vesselFile);
-                    bool VesselIsDebris = false;
-                    using (StreamReader sr = new StreamReader(vesselFile))
+                    //Check if valid file
+                    if (vesselID.Length == 36)
                     {
-                        string currentLine = sr.ReadLine();
-                        while (currentLine != null && !VesselIsDebris)
-                        {
-                            string trimmedLine = currentLine.Trim();
-                            if (trimmedLine.Trim().StartsWith("type =", StringComparison.Ordinal))
-                            {
-                                string VesselFrequency = trimmedLine.Substring(trimmedLine.IndexOf("=", StringComparison.Ordinal) + 2);
-                                if (VesselFrequency == "Debris")
-                                {
-                                    VesselIsDebris = true;
-                                }
-                            }
-                            currentLine = sr.ReadLine();
-                        }
-                    }
-                    if (!VesselIsDebris)
-                    {
-                        NormalVesselsList = NormalVesselsList + "[" + GetVesselValue(vesselFile, "lat") + "," + GetVesselValue(vesselFile, "lon") + "," + GetVesselValue(vesselFile, "REF") + "," + GetVesselValue(vesselFile, "hgt") + "/" + GetVesselValue(vesselFile, "alt") + "," + "\"?\"" + "," + GetVesselValue(vesselFile, "name") + "," + GetVesselValue(vesselFile, "type") + "],";
-                    }
-                    else
-                    {
-                        DebrisVesselsList = DebrisVesselsList + "[" + GetVesselValue(vesselFile, "lat") + "," + GetVesselValue(vesselFile, "lon") + "," + GetVesselValue(vesselFile, "REF") + "," + GetVesselValue(vesselFile, "hgt") + "/" + GetVesselValue(vesselFile, "alt") + "," + "\"?\"" + "," + GetVesselValue(vesselFile, "name") + "," + GetVesselValue(vesselFile, "type") + "],";
+                        VesselsList = VesselsList + "[" + GetVesselValue(vesselFile, "lat") + "," + GetVesselValue(vesselFile, "lon") + "," + GetVesselValue(vesselFile, "REF") + "," + GetVesselValue(vesselFile, "hgt") + "/" + GetVesselValue(vesselFile, "alt") + "," + "\"?\"" + "," + GetVesselValue(vesselFile, "name") + "," + GetVesselValue(vesselFile, "type") + ",\"" + vesselID + "\"],";
                     }
                 }
                 //TODO: use actual JSON, instead of creating and sending a string.
-                NormalVesselsList = NormalVesselsList.Remove(NormalVesselsList.Length - 1, 1) + "]}";
-                DebrisVesselsList = DebrisVesselsList.Remove(DebrisVesselsList.Length - 1, 1) + "]}";
-                FinalSentVesselsList = "{\"Main\":{" + DebrisVesselsList + NormalVesselsList + "}}";
+                VesselsList = VesselsList.Remove(VesselsList.Length - 1, 1) + "]}";
+                FinalSentVesselsList = VesselsList + "}";
+                //Sometimes it would send every \, I have no idea why it happens, but this should remove them from the JSON before it sends.
+                FinalSentVesselsList.Replace("\\", string.Empty);
                 //TODO: send the request in C# instead of launching an executable.
                 Process proc = new Process
                 {
