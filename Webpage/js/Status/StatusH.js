c7532006accf8bd9149aee2180219a72b5c3e162
FrostBird347 <39435218+FrostBird347@users.noreply.github.com>
2020-04-13 14:58:14 +0800

    Improved page height detection

--- Webpage/js/Status/StatusH.js
+++ Webpage/js/Status/StatusH.js
@@ -1,7 +1,7 @@
 var StatusH2 = "1000px";
 function StatusH(){
 	var body = document.body, html = document.documentElement;
-	var height = Math.min( body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight );
+	var height = Math.max( body.scrollHeight, body.offsetHeight, html.clientHeight, html.offsetHeight );
     if (height > 0) {
     	StatusH2 = Math.floor ( height - 10 ) + "px"
     }
