7bc91889b7cc44bbf5d8a8d0d4f522c1135d6254
FrostBird347 <39435218+FrostBird347@users.noreply.github.com>
2020-06-26 12:34:36 +0800

    Issue #14 fix

--- Webpage/js/Map/GetOpenPlanet.js
+++ Webpage/js/Map/GetOpenPlanet.js
@@ -2,62 +2,62 @@ function GetOpenPlanet() {
 	var FinalPlanet = "Kerbin",
 	PlanetURL = document.getElementsByClassName("leaflet-control-celestialbodies-toggle")[0].style.backgroundImage
 	
-	switch (PlanetURL) {
-		case 'url("img/planets/body-eeloo.png")':
+	switch (true) {
+		case PlanetURL.includes('body-eeloo.png'):
 			FinalPlanet = 'Eeloo';
 			break;
-		case 'url("img/planets/body-kerbin.png")':
+		case PlanetURL.includes('body-kerbin.png'):
 			FinalPlanet = 'Kerbin';
 			break;
-		case 'url("img/planets/body-duna.png")':
+		case PlanetURL.includes('body-duna.png'):
 			FinalPlanet = 'Duna';
 			break;
-		case 'url("img/planets/body-kerbol.png")':
+		case PlanetURL.includes('body-kerbol.png'):
 			FinalPlanet = 'Kerbol';
 			break;
-		case 'url("img/planets/body-moho.png")':
+		case PlanetURL.includes('body-moho.png'):
 			FinalPlanet = 'Moho';
 			break;
-		case 'url("img/planets/body-bop.png")':
+		case PlanetURL.includes('body-bop.png'):
 			FinalPlanet = 'Bop';
 			break;
-		case 'url("img/planets/body-dres.png")':
+		case PlanetURL.includes('body-dres.png'):
 			FinalPlanet = 'Dres';
 			break;
-		case 'url("img/planets/body-duna.png")':
+		case PlanetURL.includes('body-duna.png'):
 			FinalPlanet = 'Duna';
 			break;
-		case 'url("img/planets/body-eve.png")':
+		case PlanetURL.includes('body-eve.png'):
 			FinalPlanet = 'Eve';
 			break;
-		case 'url("img/planets/body-gilly.png")':
+		case PlanetURL.includes('body-gilly.png'):
 			FinalPlanet = 'Gilly';
 			break;
-		case 'url("img/planets/body-ike.png")':
+		case PlanetURL.includes('body-ike.png'):
 			FinalPlanet = 'Ike';
 			break;
-		case 'url("img/planets/body-jool.png")':
+		case PlanetURL.includes('body-jool.png'):
 			FinalPlanet = 'Jool';
 			break;
-		case 'url("img/planets/body-laythe.png")':
+		case PlanetURL.includes('body-laythe.png'):
 			FinalPlanet = 'Laythe';
 			break;
-		case 'url("img/planets/body-minmus.png")':
+		case PlanetURL.includes('body-minmus.png'):
 			FinalPlanet = 'Minmus';
 			break;
-		case 'url("img/planets/body-moho.png")':
+		case PlanetURL.includes('body-moho.png'):
 			FinalPlanet = 'Moho';
 			break;
-		case 'url("img/planets/body-mun.png")':
+		case PlanetURL.includes('body-mun.png'):
 			FinalPlanet = 'Mun';
 			break;
-		case 'url("img/planets/body-pol.png")':
+		case PlanetURL.includes('body-pol.png'):
 			FinalPlanet = 'Pol';
 			break;
-		case 'url("img/planets/body-tylo.png")':
+		case PlanetURL.includes('body-tylo.png'):
 			FinalPlanet = 'Tylo';
 			break;
-		case 'url("img/planets/body-vall.png")':
+		case PlanetURL.includes('body-vall.png'):
 			FinalPlanet = 'Vall';
 			break;
 	}
